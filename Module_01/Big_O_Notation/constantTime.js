40279009b50705dc8bf443aa4a2874a6da522795
Ekhlasur Rahman <ekhlasur.ru@outlook.com>
2025-11-01 22:00:53 +0600

    add index  file

--- Module_01/Big_O_Notation/constantTime.js
+++ Module_01/Big_O_Notation/constantTime.js
@@ -1,8 +1,15 @@
 //constant Time || order of 1 O(1)
 
-let x = 10;
-x = x + 5;
-console.log(x); //15
+// let x = 10;
+// x = x + 5;
+// console.log(x); //15
 
-let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
-console.log(arr[5]); //66
+// let arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+// console.log(arr[5]); //66
+
+
+let i = 0;         // i এর মান শুরুতে 0
+for (i; i < n; i++) {
+  console.log(i);  // i এর মান প্রিন্ট করে
+}
+
