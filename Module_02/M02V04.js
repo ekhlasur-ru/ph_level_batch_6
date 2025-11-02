0701866d7033e7b68a3e350e47468c93357bfe6e
Ekhlasur Rahman <ekhlasur.ru@outlook.com>
2025-11-03 00:00:31 +0600

    add some role bsse access and filter data show limit m02v04

---
+++ Module_02/M02V04.js
@@ -0,0 +1,37 @@
+//Role Base Access COntrol using array some and includes method
+
+// const currentUserRole = ["user", "editor"];
+// const featureUserRole = ["admin", "manager"];
+// const canAccess = currentUserRole.some((rule) =>
+//   featureUserRole.includes(rule)
+// );
+// console.log(canAccess);  //false
+
+const currentUserRole = ["user", "editor", "admin"];
+const featureUserRole = ["admin", "manager"];
+const canAccess = currentUserRole.some((rule) =>
+  featureUserRole.includes(rule)
+);
+console.log(canAccess); //true
+
+//array.from()
+
+// const arr = Array.from({ length: 10 });
+// console.log(arr);  //
+
+const arr = Array.from({ length: 10 }).fill(null);
+//   .map((_, index) => index + 1);
+console.log(arr);
+
+const Arr = Array.from(
+  [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+  (value, i) => value * value
+);
+console.log(Arr);
+
+const rang = (start, stop, step) =>
+  Array.from(
+    { length: Math.ceil((stop - start) / step) + 1 },
+    (_, i) => start + i * step
+  );
+console.log(rang(10, 100, 5));
